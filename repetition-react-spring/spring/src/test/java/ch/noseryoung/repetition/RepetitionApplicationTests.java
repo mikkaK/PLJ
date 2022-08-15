15b570f5d57c5a73c26136a8eb5ddd46773934d2
MikkaK <mikka.kummer@noseryoung.ch>
2022-08-15 13:39:52 +0200

    initial commit

---
+++ repetition-react-spring/spring/src/test/java/ch/noseryoung/repetition/RepetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.repetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepetitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
